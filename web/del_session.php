825c59d28b6223a28abac72f460ae0834b85aae2
adel-s <adel.sachkov@gmail.com>
2017-04-01 16:40:00 +0300

    There\'s no need to free result after delete - it\'s always TRUE or FALSE but not result object.

--- web/del_session.php
+++ web/del_session.php
@@ -14,13 +14,8 @@ elseif (isset($_GET["deletesession"])) {
 if (isset($deletesession) && !empty($deletesession)) {
     $delresult = mysqli_query($con, "DELETE FROM $db_table
                           WHERE session=".quote_value($deletesession)) or die(mysqli_error($con));
-
-    mysqli_free_result($delresult);
-
     $delresult = mysqli_query($con, "DELETE FROM $db_sessions_table
                           WHERE session=".quote_value($deletesession)) or die(mysqli_error($con));
-
-    mysqli_free_result($delresult);
 }
 //echo "<!-- End del_session.php at ".date("H:i:s", microtime(true))." -->\r\n";
 ?>
